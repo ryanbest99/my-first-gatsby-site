43f24e85f0fe7e66598f6d1713680eae59691cc3
Ryan Park <ryan.developer99@gmail.com>
2021-07-06 15:43:55 +0900

    Finished Gatsby Tutorial Part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,23 @@
-import * as React from "react";
+import React from "react";
 import { Link } from "gatsby";
 import Title from "../components/Title";
 import Layout from "../components/layout";
-import { title } from "../components/my-component.module.css";
 
-// markup
-const IndexPage = () => {
+function AboutPage() {
   return (
     <main>
-      {/* <title>Home Page</title>
-      <Title name={"Home"} />
-      <Link to="/about">About</Link>
+      {/* <title>About Me</title>
+      <Title name={"About Me"} />
+      <Link to="/">Back to Home</Link>
 
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p> */}
-      <Layout pageTitle="Home Page">
+      <Layout pageTitle="About Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
       </Layout>
     </main>
   );
-};
+}
 
-export default IndexPage;
+export default AboutPage;
